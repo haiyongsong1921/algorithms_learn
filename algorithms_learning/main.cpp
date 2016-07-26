f3601ab6496e6ead0292533811d4810c30d9e58f
haiyong <sunhy339@126.com>
2016-07-26 21:59:19 +0800

    Initial Commit

---
+++ algorithms_learning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithms_learning
+//
+//  Created by Adam Sun on 16/7/26.
+//  Copyright © 2016年 Adam Sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
